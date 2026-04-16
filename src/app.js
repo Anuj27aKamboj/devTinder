9d6a82663338bc1fb6015d168f00cd8b53b8dded
Anuj Kamboj <anujkamboj.27@Anujs-MacBook-Air.local>
2026-04-17 00:41:17 +0530

    Fix CORS and production config

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin: "http://3.26.43.77",
   credentials: true,
 }));
 app.use(express.json());
